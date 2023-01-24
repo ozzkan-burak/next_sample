81b801ea160360e79f2db3c3b12d5f762cea675b
Burak Özkan <burak.ozkan@flo.com.tr>
2023-01-24 23:12:53 +0300

    add babel-styled-component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+
+import styled from "styled-components";
+
 import FooterColum from './FooterColum';
 import Typo from '../Typo';
+
+
 
 const Footer = () => {
   return (
